a1c50a1b964d87559550cec4c67ba42489fac454
weirdmustard <samael.schroeder@mail.de>
2024-04-19 10:01:25 +0200

    Test-push

---
+++ src/git_example_FUEM1/SamTest.java
@@ -0,0 +1,11 @@
+package git_example_FUEM1;
+
+public class SamTest {
+
+	//Ihr Paket ist auf dem Weg und wird zwischen 08:00 und 22:00 zugestellt
+	
+	public static void main(String[] args) {
+
+	}
+
+}
