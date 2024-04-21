8d52ef2ca6d22b9bb8012938d1a2f8a203b53a3c
Normann <Normann@DESKTOP-Q068Q08>
2024-04-21 22:54:27 +0200

    hab nen bug

---
+++ src/Game/StartGame.java
@@ -0,0 +1,17 @@
+package Game;
+
+public class StartGame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+	SpielCode HM = new SpielCode();
+	
+	HM.Hangman();
+		
+		
+	}
+
+}
