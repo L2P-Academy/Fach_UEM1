1a12a7fe07d910fd8d5d1687717101809c9ac153
Grrouti <maximilian_bernatek@live.de>
2024-04-22 08:32:12 +0200

    Neues Package "GUI" angelegt.

---
+++ src/GUI/Main_Window.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+
+package GUI;
+
+
+public class Main_Window extends JFrame  {
+
+	JFrame mainPanel = new JFrame("Anzeige");
+	meinJFrame.setSize(400, 400);
+	JPanel panel = new JPanel();
+	JLabel label = new JLabel("Sie Sie das ?");
+	JTextField tfName = new JTextField("Max Mustermann", 33);
+	tfName.setForeground(Color.BLUE);
+	tfName.setBackground(Color.YELLOW);
+	panel.add(ftName);
+	JButton button = new JButton("Ja");
+	panel.add(button);
+	mainPanel.add(Panel);
+	
+	mainFrame.setVisible(true);
+}
