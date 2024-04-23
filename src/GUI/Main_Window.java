672596606ba9ef08b0e26d0297bd807337b9b61a
Grrouti <maximilian_bernatek@live.de>
2024-04-23 09:36:13 +0200

    1. res ordner eingefügt 2.

--- src/GUI/Main_Window.java
+++ src/GUI/Main_Window.java
@@ -4,19 +4,40 @@ import java.awt.Color;
 
 import javax.swing.*;
 
-public class Main_Window extends JFrame {
+public class Gui_Hangman extends JFrame {
+/*	
+		//Variablen
+	private JButton startGame;
+	private int scaledWidth,scaledHeight;
+	private JPanel panelBildHangman;
+	private JPanel panelBtnPruefen;
+	private JLabel resImageHangman;
+	private JTextField wordToLookFor;
+	private String[] imageHangmanPath;
 
-	public Main_Window() {
-		
+	public HangmanView() {
 		// Hier Fenster initialisieren -> Title, Size, CloseOperation
 		
-		// Objeke mit "new"
+			setTitle("Hangman");
+			setSize(1200,900);
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}	
+			
+		// Panel für den Hangman
+			
+			panelBildHangman = new JPanel ();
+			
+			
+			
 		
+		// Objeke mit "new"
+	imageHangmanPath = new String[] {"res/Hangman_1_8.jpg","res/Hangman_2_8.jpg","res/Hangman_3_8.jpg","res/Hangman_4_8.jpg","res/Hangman_5_8.jpg","res/Hangman_6_8.jpg","res/Hangman_7_8.jpg","res/Hangman_8_8.jpg"};
+	startGame = new jButton("Neues Spiel Starten");
+	
 		// Hinzufügen von Objekten
 		
 		// getContentPane und setVisible (am Ende)
 		
-		this.setSize(400, 400);
 		JPanel panel = new JPanel();
 		JLabel label = new JLabel("Sie Sie das ?");
 		JTextField tfName = new JTextField("Max Mustermann", 33);
@@ -26,5 +47,5 @@ public class Main_Window extends JFrame {
 		JButton button = new JButton("Ja");
 		panel.add(button);
 	}
-
+*/
 }
